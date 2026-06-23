40badc6ff4a6110892f3a711b2113be8069009b0
Sk-rose-2007 <roshantpms2007@gmail.com>
2026-06-23 18:04:56 +0530

    day2 workouts

---
+++ DAY2/multiply.py
@@ -0,0 +1,5 @@
+a=int(input())
+for i in range (a,a+1):
+    for j in range(1,11):
+        n=i*j
+        print(j,"*",i,"=",n)
---
+++ DAY2/vowel.py
@@ -0,0 +1,6 @@
+letter=input("enter a letter: ")
+vowel=['a','e','i','o','u']
+if letter in vowel:
+    print("Vowel")
+else:
+    print("Consonant")
